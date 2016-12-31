ee1513ad5178b6b81e621623505923179ed2d59e
Filippo Squillace <squillace.filippo@gmail.com>
2016-12-31 17:22:01 +0000

    Issue #13: Add integration test for vim config

--- tests/integ-tests/integ-tests.sh
+++ tests/integ-tests/integ-tests.sh
@@ -9,3 +9,9 @@ echo "alias q='exit'" > ~/.config/kyrat/bashrc
 ./bin/kyrat -v -o "StrictHostKeyChecking no" localhost -- ls -lh
 ./bin/kyrat -v -o "StrictHostKeyChecking no" localhost -- [[ ! -z "\\\$INPUTRC" ]]
 ./bin/kyrat -v -o "StrictHostKeyChecking no" localhost -- [[ ! -z "\\\$VIMINIT" ]]
+
+
+echo -e "let myvariable=10\nlet myvariable\nq" > ~/.config/kyrat/vimrc
+VIM_OUTPUT="$(./bin/kyrat -v -o "StrictHostKeyChecking no" localhost -- vim)"
+
+echo "$VIM_OUTPUT" | grep myvariable || { echo "vimrc has not been loaded properly"; exit 1; }
